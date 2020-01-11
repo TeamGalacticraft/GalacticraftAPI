7f1cee0f5e1c87b14edc3f980993faeeb6684db1
Joe van der Zwet <me@joezwet.me>
2020-01-12 12:22:15 +1300

    :bug: Fix builder translation key, closes #4

--- src/main/java/com/hrznstudio/galacticraft/api/celestialbodies/CelestialBodyType.java
+++ src/main/java/com/hrznstudio/galacticraft/api/celestialbodies/CelestialBodyType.java
@@ -204,7 +204,7 @@ public class CelestialBodyType implements DynamicSerializable {
         }
 
         public Builder translationKey(String key) {
-            this.translationKey = translationKey;
+            this.translationKey = key;
             return this;
         }
 
