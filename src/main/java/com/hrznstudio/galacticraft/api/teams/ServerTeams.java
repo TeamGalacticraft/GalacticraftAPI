c9e7ed1910fff2ee9c2edd03ac51a1ab7b3359be
Joe van der Zwet <me@joezwet.me>
2020-02-20 19:21:07 +1300

    :bug: Fix crash with ServerTeams' ArrayOutOfBoundsException.

--- src/main/java/com/hrznstudio/galacticraft/api/teams/ServerTeams.java
+++ src/main/java/com/hrznstudio/galacticraft/api/teams/ServerTeams.java
@@ -1,22 +1,20 @@
 package com.hrznstudio.galacticraft.api.teams;
 
-import net.minecraft.network.Packet;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.network.ServerPlayerEntity;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ServerTeams extends Teams {
     private MinecraftServer server;
-    private Runnable[] listeners = new Runnable[0];
+    private List<Runnable> listeners = new ArrayList<>();
 
     public ServerTeams(MinecraftServer server) {
         this.server = server;
     }
 
     public void addListener(TeamsSync teamsSync) {
-        this.listeners = Arrays.copyOf(this.listeners, this.listeners.length);
-        this.listeners[this.listeners.length - 1] = teamsSync;
+        this.listeners.add(teamsSync);
     }
 
     private void runListeners() {
