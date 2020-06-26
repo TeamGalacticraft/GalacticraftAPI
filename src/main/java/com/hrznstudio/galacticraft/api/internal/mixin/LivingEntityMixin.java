cb1866b9e9a1ed616dd205cbe1ffada8047cee82
Brendan Lewis <brendanelewis@outlook.com>
2020-06-26 16:39:04 -0400

    Add new fall damage mixin for planet gravity
    This should work for every celestial body, dynamically. Because of how the calculation is set up, Earth fall damage stays the same.

--- src/main/java/com/hrznstudio/galacticraft/api/internal/mixin/LivingEntityMixin.java
+++ src/main/java/com/hrznstudio/galacticraft/api/internal/mixin/LivingEntityMixin.java
@@ -4,10 +4,18 @@ import com.hrznstudio.galacticraft.api.celestialbodies.CelestialBodyType;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Optional;
 
@@ -22,5 +30,23 @@ public abstract class LivingEntityMixin extends Entity {
     private double modifyGravity(double d) {
         Optional<CelestialBodyType> type = CelestialBodyType.getByDimType(((LivingEntity)(Object)this).world.getRegistryKey());
         return type.map(celestialBodyType -> celestialBodyType.getGravity() * 0.08d).orElse(0.08d);
+    }
+
+    @Shadow
+    protected abstract int getNextAirUnderwater(int air);
+
+    @Shadow
+    public abstract StatusEffectInstance getStatusEffect(StatusEffect effect);
+
+    @Inject(method = "computeFallDamage", at = @At("HEAD"), cancellable = true)
+    protected void onComputeFallDamage(float fallDistance, float damageMultiplier, CallbackInfoReturnable<Integer> cir) {
+        RegistryKey<World> worldRegistryKey = this.world.getRegistryKey();
+        for (CelestialBodyType body : CelestialBodyType.getAll()) {
+            if (body.getWorld() == worldRegistryKey) {
+                StatusEffectInstance statusEffectInstanc = this.getStatusEffect(StatusEffects.JUMP_BOOST);
+                float ff = statusEffectInstanc == null ? 0.0F : (float) (statusEffectInstanc.getAmplifier() + 6);
+                cir.setReturnValue(MathHelper.ceil(((fallDistance / (1 / body.getGravity())) - 3.0F - ff) * damageMultiplier));
+            }
+        }
     }
 }
