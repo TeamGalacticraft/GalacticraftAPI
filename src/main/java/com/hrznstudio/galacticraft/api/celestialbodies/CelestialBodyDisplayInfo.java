f2ad8a3093bbdf496e87eb063272f2554b1a50b7
marcus8448 <28928887+marcus8448@users.noreply.github.com>
2020-10-12 16:47:32 -0700

    chore: add more data to CelestialBodyDisplayInfo

--- src/main/java/com/hrznstudio/galacticraft/api/celestialbodies/CelestialBodyDisplayInfo.java
+++ src/main/java/com/hrznstudio/galacticraft/api/celestialbodies/CelestialBodyDisplayInfo.java
@@ -26,8 +26,13 @@ import net.minecraft.util.Identifier;
 
 public class CelestialBodyDisplayInfo {
 
-    private final double orbitDistance;
     private final double orbitTime;
+    private final double orbitOffsetX;
+    private final double orbitOffsetY;
+    private final double orbitRadX;
+    private final double orbitRadY;
+    private final double scale;
+
     private final Identifier iconTexture;
     private final int iconX;
     private final int iconY;
@@ -36,17 +41,26 @@ public class CelestialBodyDisplayInfo {
 
     /**
      * Display information for celestial bodies
-     * @param orbitDistance Distance away from parent body {@link CelestialBodyType#getParent()}
      * @param orbitTime The time it takes for the body to complete a single orbit (in ticks)
+     * @param orbitOffsetX The distance on the X axis from the parent planet
+     * @param orbitOffsetY The distance on the Y axis from the parent planet
+     * @param orbitRadX The planet's orbit radius on the X axis
+     * @param orbitRadY The planet's orbit radius on the Y axis
+     * @param scale The planet's scale, relative to earth
      * @param iconTexture Identifier for body's icon texture
      * @param iconX X chord in icon texture
      * @param iconY Y chord in icon texture
      * @param iconW Width of icon in texture
      * @param iconH Height of icon in texture
      */
-    public CelestialBodyDisplayInfo(double orbitDistance, double orbitTime, Identifier iconTexture, int iconX, int iconY, int iconW, int iconH) {
-        this.orbitDistance = orbitDistance;
+    public CelestialBodyDisplayInfo(double orbitTime, double orbitOffsetX, double orbitOffsetY, double orbitRadX, double orbitRadY, double scale, Identifier iconTexture, int iconX, int iconY, int iconW, int iconH) {
         this.orbitTime = orbitTime;
+        this.orbitOffsetX = orbitOffsetX;
+        this.orbitOffsetY = orbitOffsetY;
+        this.orbitRadX = orbitRadX;
+        this.orbitRadY = orbitRadY;
+        this.scale = scale;
+
         this.iconTexture = iconTexture;
         this.iconX = iconX;
         this.iconY = iconY;
@@ -54,12 +68,28 @@ public class CelestialBodyDisplayInfo {
         this.iconH = iconH;
     }
 
-    public double getOrbitDistance() {
-        return orbitDistance;
-    }
-
     public double getOrbitTime() {
         return orbitTime;
+    }
+
+    public double getOrbitOffsetX() {
+        return orbitOffsetX;
+    }
+
+    public double getOrbitOffsetY() {
+        return orbitOffsetY;
+    }
+
+    public double getOrbitRadX() {
+        return orbitRadX;
+    }
+
+    public double getOrbitRadY() {
+        return orbitRadY;
+    }
+
+    public double getScale() {
+        return scale;
     }
 
     public Identifier getIconTexture() {
@@ -82,27 +112,53 @@ public class CelestialBodyDisplayInfo {
         return iconH;
     }
 
+
     public static class Builder {
-        private Identifier iconTexture = null;
-        private double orbitDistance = 0d;
         private double orbitTime = 0d;
+        private double orbitOffsetX = 0.0d;
+        private double orbitOffsetY = 0.0d;
+        private double orbitRadX = 25.0d;
+        private double orbitRadY = 25.0d;
+        private double scale = 1.0d;
+
+        private Identifier iconTexture = null;
         private int iconX = 0;
         private int iconY = 0;
         private int iconW = 16;
         private int iconH = 16;
 
-        public Builder texture(Identifier texture) {
-            this.iconTexture = texture;
-            return this;
-        }
-
-        public Builder distance(double orbitDistance) {
-            this.orbitDistance = orbitDistance;
-            return this;
-        }
-
         public Builder time(double time) {
             this.orbitTime = time;
+            return this;
+        }
+
+        public Builder offsetX(int offX) {
+            this.orbitOffsetX = offX;
+            return this;
+        }
+
+        public Builder offsetY(int offY) {
+            this.orbitOffsetY = offY;
+            return this;
+        }
+
+        public Builder radX(int radX) {
+            this.orbitRadX = radX;
+            return this;
+        }
+
+        public Builder radY(int radY) {
+            this.orbitRadY = radY;
+            return this;
+        }
+
+        public Builder scale(double scale) {
+            this.scale = scale;
+            return this;
+        }
+
+        public Builder texture(Identifier texture) {
+            this.iconTexture = texture;
             return this;
         }
 
@@ -127,7 +183,8 @@ public class CelestialBodyDisplayInfo {
         }
 
         public CelestialBodyDisplayInfo build() {
-            return new CelestialBodyDisplayInfo(this.orbitDistance, this.orbitTime, this.iconTexture, this.iconX, this.iconY, this.iconW, this.iconH);
+            if (this.iconTexture == null) throw new NullPointerException("Tried to create display info without icon!");
+            return new CelestialBodyDisplayInfo(this.orbitTime, this.orbitOffsetX, this.orbitOffsetY, this.orbitRadX, this.orbitRadY, this.scale, this.iconTexture, this.iconX, this.iconY, this.iconW, this.iconH);
         }
     }
 }
