1b30f8f15b9a3c0b5ae96d0f38ef8ac5a57ee998
marcus8448 <28928887+marcus8448@users.noreply.github.com>
2021-06-05 11:38:59 -0700

    fix: mixin inject targets

--- src/main/java/dev/galacticraft/impl/internal/mixin/DynamicRegistryManagerMixin.java
+++ src/main/java/dev/galacticraft/impl/internal/mixin/DynamicRegistryManagerMixin.java
@@ -46,20 +46,20 @@ public abstract class DynamicRegistryManagerMixin {
 	@Unique
 	private static boolean override = false;
 
-	@Inject(method = "method_31141", at = @At(value = "INVOKE", target = "Ljava/lang/Object;equals(Ljava/lang/Object;)Z", shift = At.Shift.AFTER, ordinal = 1))
-	private static <E> void testGC(DynamicRegistryManager.Impl impl, RegistryOps.EntryLoader.Impl impl2, DynamicRegistryManager.Info<E> info, CallbackInfo ci) {
+	@Inject(method = "method_31141", at = @At(value = "HEAD"))
+	private static <E> void testForSync_gc(DynamicRegistryManager.Impl registryManager, RegistryOps.EntryLoader.Impl entryLoader, DynamicRegistryManager.Info<E> info, CallbackInfo ci) {
 		RegistryKey<? extends Registry<E>> registryKey = info.getRegistry();
 		override = (registryKey.equals(AddonRegistry.ATMOSPHERIC_GAS_KEY) || registryKey.equals(AddonRegistry.GALAXY_KEY) || registryKey.equals(AddonRegistry.CELESTIAL_BODY_KEY) || registryKey.equals(AddonRegistry.ROCKET_PART_KEY));
 	}
 
-	@ModifyVariable(method = "method_31141", at = @At(value = "INVOKE", target = "Ljava/util/Map$Entry;getValue()Ljava/lang/Object;", ordinal = 0), name = "bl", ordinal = 0, index = 4)
-	private static boolean modifyGC(boolean b) {
-		return b && !override;
+	@ModifyVariable(method = "method_31141", at = @At(value = "STORE", ordinal = 0), name = "bl", ordinal = 0, index = 4)
+	private static boolean modifyForSync_gc(boolean bl) {
+		return bl && !override;
 	}
 
 	@SuppressWarnings("UnresolvedMixinReference")
 	@Inject(method = "method_30531", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/registry/DynamicRegistryManager;register(Lcom/google/common/collect/ImmutableMap$Builder;Lnet/minecraft/util/registry/RegistryKey;Lcom/mojang/serialization/Codec;)V", ordinal = 0), locals = LocalCapture.CAPTURE_FAILHARD) //synthetic method
-	private static void registerCustom(CallbackInfoReturnable<ImmutableMap<RegistryKey<? extends Registry<?>>, DynamicRegistryManager.Info<?>>> ci, ImmutableMap.Builder<RegistryKey<? extends Registry<?>>, DynamicRegistryManager.Info<?>> builder) {
+	private static void registerCustomRegistries_gc(CallbackInfoReturnable<ImmutableMap<RegistryKey<? extends Registry<?>>, DynamicRegistryManager.Info<?>>> ci, ImmutableMap.Builder<RegistryKey<? extends Registry<?>>, DynamicRegistryManager.Info<?>> builder) {
 		builder.put(AddonRegistry.ATMOSPHERIC_GAS_KEY, new DynamicRegistryManager.Info<>(AddonRegistry.ATMOSPHERIC_GAS_KEY, AtmosphericGas.CODEC, AtmosphericGas.CODEC));
 		builder.put(AddonRegistry.GALAXY_KEY, new DynamicRegistryManager.Info<>(AddonRegistry.GALAXY_KEY, Galaxy.CODEC, Galaxy.CODEC));
 		builder.put(AddonRegistry.CELESTIAL_BODY_KEY, new DynamicRegistryManager.Info<>(AddonRegistry.CELESTIAL_BODY_KEY, CelestialBody.CODEC, CelestialBody.CODEC));
